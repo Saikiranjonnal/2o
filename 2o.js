a6d951e431829cff342c46827522704d677403c9
Saikiranjonnal <jonnalasaikiran78@gmail.com>
2024-10-16 16:44:08 +0530

    Add files via upload

---
+++ 2o.js
@@ -0,0 +1,36 @@
+//Numbers methods
+//toFixed():
+
+let num=3.144444;
+console.log(num.toFixed(2));
+
+//toPrecision():
+ let num1=12345;
+ console.log(num1.toPrecision(2))
+
+ //to string():
+
+ let num2=100
+ console.log(num2.toString())
+
+//parseInt():
+
+let num3="100"
+console.log(parseInt(num3))
+
+//parseFloat():
+
+let num4='3.1333'
+console.log(parseFloat(num4))
+
+//NaN-Not-a-Number()
+
+console.log(isNaN(100))
+console.log(isNaN('hello'))
+console.log(isNaN(3.14))
+
+//isFinite()
+a=100
+console.log(isFinite(a))
+b="hello"
+console.log(isFinite(b))
